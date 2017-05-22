22cb9a2fb680672de9c9ff99aa5ea1cd6455c56c
beautifulxiaojian <xiaojiannangkang@126.com>
2017-05-22 08:25:13 +0800

    Default Theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public isCollapsed:boolean = false;
+  showMenu(){
+    alert(11);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { CollapseModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { CenterComponent } from './center/center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenterComponent,
   ],
   imports: [
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'center', pathMatch: 'full' },
+      { path: 'center', component: CenterComponent }
+    ]),
     CollapseModule.forRoot(),
     BrowserModule,
     FormsModule,
